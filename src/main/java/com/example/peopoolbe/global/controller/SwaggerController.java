656bf4bb87ea8002dd19cb8e5db11afdde4ad1e7
Park, Daekyeong <pdk2037@gmail.com>
2025-07-05 18:56:35 +0900

    Update Annotation

--- src/main/java/com/example/peopoolbe/global/controller/SwaggerController.java
+++ src/main/java/com/example/peopoolbe/global/controller/SwaggerController.java
@@ -1,9 +1,9 @@
 package com.example.peopoolbe.global.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SwaggerController {
     @RequestMapping("/")
     public String homeRedirect() {
--- src/main/java/com/example/peopoolbe/member/api/MemberController.java
+++ src/main/java/com/example/peopoolbe/member/api/MemberController.java
@@ -12,14 +12,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 

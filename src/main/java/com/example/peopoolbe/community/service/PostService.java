ad1d10c9126a7cf890e6d55cd116269f861ce47d
Park, Daekyeong <pdk2037@gmail.com>
2025-08-25 17:01:24 +0900

    Feat(#4) 리스트 조회 내림차순 변경

--- src/main/java/com/example/peopoolbe/community/service/PostService.java
+++ src/main/java/com/example/peopoolbe/community/service/PostService.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -84,7 +85,7 @@ public class PostService {
     }
 
     public PostListRes getPostList(String word, int page, int size, String startDate, String endDate) {
-        Pageable pageable = PageRequest.of(page-1, size);
+        Pageable pageable = PageRequest.of(page-1, size, Sort.by("id").descending());
         Page<Post> postPage;
 
         LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));

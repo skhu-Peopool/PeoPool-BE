79d0d852310157e812ec70a5d4c76c7d1be6bcba
Park, Daekyeong <pdk2037@gmail.com>
2025-08-25 23:53:09 +0900

    Feat(#1) 모집 상태에서 STOPPED 제거

--- src/main/java/com/example/peopoolbe/community/domain/Status.java
+++ src/main/java/com/example/peopoolbe/community/domain/Status.java
@@ -3,7 +3,6 @@ package com.example.peopoolbe.community.domain;
 public enum Status {
     UPCOMING, //모집 예정
     RECRUITING, // 모집 중
-    RECRUITED, // 모집 완료
-    STOPPED, // 모집 중지
+    RECRUITED, // 모집 마감
     UNDER_REVIEW // 검토 중
 }

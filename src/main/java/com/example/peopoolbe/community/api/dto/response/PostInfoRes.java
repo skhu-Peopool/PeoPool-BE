520efc8e645137b7f0817b716bdee0ab8af6cd36
Park, Daekyeong <pdk2037@gmail.com>
2025-08-25 23:56:04 +0900

    Feat(#4) 게시글 상세 조회에 작성일, 수정일 추가

--- src/main/java/com/example/peopoolbe/community/api/dto/response/PostInfoRes.java
+++ src/main/java/com/example/peopoolbe/community/api/dto/response/PostInfoRes.java
@@ -7,10 +7,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Builder
 public record PostInfoRes(
         Long id,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
         String title,
         String content,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul") LocalDate startDate,
@@ -25,6 +28,8 @@ public record PostInfoRes(
     public static PostInfoRes from(Post post) {
         return PostInfoRes.builder()
                 .id(post.getId())
+                .createdAt(post.getCreatedAt())
+                .updatedAt(post.getUpdatedAt())
                 .title(post.getTitle())
                 .content(post.getContent())
                 .startDate(post.getRecruitmentStartDate())

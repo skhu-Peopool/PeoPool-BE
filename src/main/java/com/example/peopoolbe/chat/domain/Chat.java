b2d32c9fff39447fd1c017fa6f9b689f635f0605
Park, Daekyeong <pdk2037@gmail.com>
2025-09-24 00:26:33 +0900

    Feat(#14) 채팅 기본적인 틀 구현

---
+++ src/main/java/com/example/peopoolbe/chat/domain/Chat.java
@@ -0,0 +1,40 @@
+package com.example.peopoolbe.chat.domain;
+
+import com.example.peopoolbe.global.entity.BaseEntity;
+import com.example.peopoolbe.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Chat extends BaseEntity {
+
+    @Column(nullable = false, length = 1000)
+    private String message;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Member sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatroom_id", nullable = false)
+    private ChatRoom chatroom;
+
+    private boolean isRead = false;
+
+    @Builder
+    public Chat(String message, Member sender, Member receiver, ChatRoom chatroom, boolean isRead) {
+        this.message = message;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.chatroom = chatroom;
+        this.isRead = isRead;
+    }
+}
